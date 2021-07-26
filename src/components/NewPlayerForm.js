c1d3810c12e6af9c72ccc0c30cfe85a6e4d4baac
Dave Butler <davebutlerid@gmail.com>
2021-07-25 19:11:16 -0600

    add NewPlayerForm component v1

---
+++ src/components/NewPlayerForm.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+export const NewPlayerForm = ( props ) => {
+    return (
+        <div>
+            <Form>
+                <Form.Group className="mb-3" controlID="newPlayerName">
+                    <Form.Label>Player Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter Player Name" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </div>
+    )
+}
